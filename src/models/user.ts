7399a09aa2c46552a9efba77e9fd3e33c6b3324d
Ruslan <35293403+happy-land@users.noreply.github.com>
2023-04-29 10:41:05 +0300

    schemas and models done

---
+++ src/models/user.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IUser {
+  name: string;
+  about: string;
+  avatar: string;
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 200,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  }
+});
+
+
+export default mongoose.model<IUser>('user', userSchema);
