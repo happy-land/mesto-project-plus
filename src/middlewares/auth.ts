dc4cefb9c094aed72ce098f64a158aa993e4396b
Ruslan <35293403+happy-land@users.noreply.github.com>
2023-05-05 15:44:25 +0300

    auth done

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import UnauthorizedError from '../errors/unauthorized-err';
 
-export default (req: Request, res: Response, next: NextFunction) => {
+interface SessionRequest extends Request {
+  user?: string | JwtPayload;
+}
 
+export default (req: SessionRequest, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+  req.user = payload;
+  return next();
 };
